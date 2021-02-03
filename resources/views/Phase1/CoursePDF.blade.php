08fdc5fa2643b486e4f69886a673d2b4e5d3be60
smuhtasim <56603189+smuhtasim@users.noreply.github.com>
2021-02-03 20:39:32 +0600

    modified the course list pdf

--- resources/views/Phase1/CoursePDF.blade.php
+++ resources/views/Phase1/CoursePDF.blade.php
@@ -2,9 +2,27 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title></title>
+    <title><b>Course List</b></title>
 </head>
+<style>
+    th,
+    td{
+        border: 1px solid black;
+        border-collapse: collapse;
+    }
+
+    th,
+    td{
+        padding: 15px;
+        text-align: center;
+    }
+
+    table{
+        width:100%;
+    }
+</style>
 <body>
+<h1 style="text-align: center">Course List</h1>
 <table class="table table-bordered">
     <thead>
     <tr>
